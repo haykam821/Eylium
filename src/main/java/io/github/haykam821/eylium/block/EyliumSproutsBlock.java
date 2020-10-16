5ccb838276aa3894c38f929da21ea743bc16d7d4
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-10-16 09:41:26 -0400

    Allow growing end sprouts into tall end sprouts

--- src/main/java/io/github/haykam821/eylium/block/EyliumSproutsBlock.java
+++ src/main/java/io/github/haykam821/eylium/block/EyliumSproutsBlock.java
@@ -1,8 +1,12 @@
 package io.github.haykam821.eylium.block;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FernBlock;
+import net.minecraft.block.TallPlantBlock;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 
@@ -14,5 +18,17 @@ public class EyliumSproutsBlock extends FernBlock {
 	@Override
 	public boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
 		return floor.isOf(ModBlocks.EYLIUM.getBlock());
+	}
+
+	private TallPlantBlock getGrowthBlock() {
+		return (TallPlantBlock) ModBlocks.TALL_END_SPROUTS.getBlock();
+	}
+
+	@Override
+	public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+		TallPlantBlock growthBlock = this.getGrowthBlock();
+		if (growthBlock.getDefaultState().canPlaceAt(world, pos) && world.isAir(pos.up())) {
+			growthBlock.placeAt(world, pos, 2);
+		}
 	}
 }
